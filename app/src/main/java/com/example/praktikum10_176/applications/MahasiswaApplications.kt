b7efb90dcf58526f963d25cd4b429c22933b1b5f
riskalaluna <riskalaluna@gmail.com>
2024-12-29 21:10:45 +0700

    Application digunakan untuk melakukan inisialisasi yang bersifat global untuk seluruh aplikasi

--- app/src/main/java/com/example/praktikum10_176/applications/MahasiswaApplications.kt
+++ app/src/main/java/com/example/praktikum10_176/applications/MahasiswaApplications.kt
@@ -1,8 +1,10 @@
 package com.example.praktikum10_176.applications
 
+import android.app.Application
+import com.example.praktikum10_176.container.AppContainer
 import com.example.praktikum10_176.container.MahasiswaContainer
 
-class MahasiswaApplications {
+class MahasiswaApplications:Application(){
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
