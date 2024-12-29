706f1e70da759fec3eefaaa0f9b1b38e873a6475
riskalaluna <riskalaluna@gmail.com>
2024-12-29 17:44:05 +0700

    OutlinedTextField untuk input nim dan jenis kelamin

--- app/src/main/java/com/example/praktikum10_176/ui/view/InsertView.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/view/InsertView.kt
@@ -32,6 +32,22 @@ fun FormInput(
             enabled = enabled,
             singleLine = true
         )
+        OutlinedTextField(
+            value = insertUiEvent.nim,
+            onValueChange = {onValueChange(insertUiEvent.copy(nim = it))},
+            label = { Text("NIM") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.jenisKelamin,
+            onValueChange = {onValueChange(insertUiEvent.copy(jenisKelamin = it))},
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 
 }
