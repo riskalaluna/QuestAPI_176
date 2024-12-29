95f98164f5963238050d3f71d7f4cc27bdded9e6
riskalaluna <riskalaluna@gmail.com>
2024-12-29 21:13:53 +0700

    menambahkan permission di AndroidManifest

--- app/src/main/java/com/example/praktikum10_176/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/PenyediaViewModel.kt
@@ -1,11 +1,10 @@
 package com.example.praktikum10_176.ui
 
-import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.praktikum10_176.applications.MahasiswaApplications
+import com.example.praktikum10_176.MahasiswaApplications
 import com.example.praktikum10_176.ui.viewmodel.HomeViewModel
 import com.example.praktikum10_176.ui.viewmodel.InsertViewModel
 
@@ -15,6 +14,5 @@ object PenyediaViewModel{
         initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
     }
 }
-
 fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
