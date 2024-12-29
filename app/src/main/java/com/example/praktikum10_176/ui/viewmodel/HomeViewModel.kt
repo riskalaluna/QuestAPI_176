ab8a27ffd1689952ffda96c0c018ec15e801e65c
riskalaluna <riskalaluna@gmail.com>
2024-12-29 14:09:13 +0700

    menambahkan fungsi getMhs untuk data mahasiswa dari RepositoryMahasiswa

--- app/src/main/java/com/example/praktikum10_176/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,41 @@
 package com.example.praktikum10_176.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.praktikum10_176.model.Mahasiswa
+import com.example.praktikum10_176.repository.RepositoryMahasiswa
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class HomeUiState{
     data class Succsess(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: RepositoryMahasiswa): ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Succsess(mhs.getMahasiswa())
+            }catch (e: IOException) {
+                HomeUiState.Error
+            }catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
 }
