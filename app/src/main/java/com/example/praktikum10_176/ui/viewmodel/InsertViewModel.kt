683eb6d10e97b8c232853744ef647c548a236155
riskalaluna <riskalaluna@gmail.com>
2024-12-29 13:47:01 +0700

    ata class dan fungsi ekstensi untuk mengelola konversi data dan model aplikasi

---
+++ app/src/main/java/com/example/praktikum10_176/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.praktikum10_176.ui.viewmodel
+
+import com.example.praktikum10_176.model.Mahasiswa
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
