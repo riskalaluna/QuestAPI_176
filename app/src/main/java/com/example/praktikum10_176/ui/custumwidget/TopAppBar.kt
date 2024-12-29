8a0ea44e7fac7bb97ae77d269fe16d0ada769d8a
riskalaluna <riskalaluna@gmail.com>
2024-12-29 14:24:05 +0700

    CenterAlignedTopAppBar dari Material 3 untuk membuat tampilan TopAppBar

--- app/src/main/java/com/example/praktikum10_176/ui/custumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/custumwidget/TopAppBar.kt
@@ -1,7 +1,15 @@
 package com.example.praktikum10_176.ui.custumwidget
 
 import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,5 +24,25 @@ fun CustumeTopAppBar(
     navigateUp:() -> Unit = {},
     onRefresh: () -> Unit = {},
 ) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        actions = {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = " ",
+                modifier = Modifier.clickable {
+                    onRefresh()
+                })
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if(canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null)
 
+                }
+            }
+        }
+    )
 }
