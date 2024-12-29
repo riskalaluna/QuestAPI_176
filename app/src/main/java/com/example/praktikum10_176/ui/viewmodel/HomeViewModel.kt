17138eb1ebd10ede9e09e6caae1d0254a74ee73e
riskalaluna <riskalaluna@gmail.com>
2024-12-29 14:12:06 +0700

    Fungsi deleteMhs bertugas untuk menghapus data mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/praktikum10_176/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/viewmodel/HomeViewModel.kt
@@ -38,4 +38,15 @@ class HomeViewModel(private val mhs: RepositoryMahasiswa): ViewModel() {
         }
     }
 
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
