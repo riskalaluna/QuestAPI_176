ded596283e84c6b3a0d54e7879776f5c609b7418
riskalaluna <riskalaluna@gmail.com>
2024-12-29 20:58:07 +0700

    mengatur alur navigasi antar halaman dalam aplikasi menggunakan NavController

---
+++ app/src/main/java/com/example/praktikum10_176/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,43 @@
+package com.example.praktikum10_176.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum10_176.ui.view.DestinasiEntry
+import com.example.praktikum10_176.ui.view.DestinasiHome
+import com.example.praktikum10_176.ui.view.EntryMhsScreen
+import com.example.praktikum10_176.ui.view.HomeScreen
+import com.example.praktikum10_176.ui.viewmodel.HomeViewModel
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
