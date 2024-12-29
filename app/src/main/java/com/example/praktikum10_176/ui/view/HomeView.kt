281c7d259edc2141f1c3d8662b234291fc34dafb
riskalaluna <riskalaluna@gmail.com>
2024-12-29 20:28:57 +0700

    menampilkan indikator loading berupa gambar

--- app/src/main/java/com/example/praktikum10_176/ui/view/HomeView.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/view/HomeView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -29,6 +30,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.praktikum10_176.R
 import com.example.praktikum10_176.model.Mahasiswa
+
+@Composable
+fun OnLoading(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading))
+}
 
 @Composable
 fun OnError(
