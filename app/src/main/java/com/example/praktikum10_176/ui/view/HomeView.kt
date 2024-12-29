2c951f857a030955a8f5a843263545af2a7a4ed4
riskalaluna <riskalaluna@gmail.com>
2024-12-29 20:36:50 +0700

    Fungsi HomeStatus sebagai handler utama menampilkan tampilan berbeda sesuai dengan status data

--- app/src/main/java/com/example/praktikum10_176/ui/view/HomeView.kt
+++ app/src/main/java/com/example/praktikum10_176/ui/view/HomeView.kt
@@ -3,10 +3,12 @@ package com.example.praktikum10_176.ui.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -30,6 +32,41 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.praktikum10_176.R
 import com.example.praktikum10_176.model.Mahasiswa
+import com.example.praktikum10_176.ui.viewmodel.HomeUiState
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when(homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Succsess ->
+            if (homeUiState.mahasiswa.isEmpty()) {
+                return Box (
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(text = "Tidak ada data Kontak")
+                }
+            } else {
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.nim)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
 
 @Composable
 fun OnLoading(
