248d532a763c322d751953fe18dc37def86ae778
riskalaluna <riskalaluna@gmail.com>
2024-12-30 09:41:31 +0700

    untuk mengelola detail informasi dari data Mahasiswa

---
+++ app/src/main/java/com/example/praktikum10_176/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.praktikum10_176.ui.viewmodel
+
+import com.example.praktikum10_176.model.Mahasiswa
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent {
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
