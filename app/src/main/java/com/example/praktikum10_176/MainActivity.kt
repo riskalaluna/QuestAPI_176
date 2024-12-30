8b797738dd7c3b9ca89d601c0b3e22bf27880e88
riskalaluna <riskalaluna@gmail.com>
2024-12-30 11:38:32 +0700

    menambahkan mahasiswaApp di MainActivity untuk menjalankan code

--- app/src/main/java/com/example/praktikum10_176/MainActivity.kt
+++ app/src/main/java/com/example/praktikum10_176/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktikum10_176.ui.MahasiswaApp
 import com.example.praktikum10_176.ui.theme.Praktikum10_176Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum10_176Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
